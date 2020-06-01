5156b2619113feda55188bf19ae3689085997de6
kc-kc <casey@theavilas.org>
2020-05-31 17:36:07 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import random
+
+TOKEN = process.env.TOKEN
+
+bot = commands.Bot(command_prefix = '_')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello there')
+
+@bot.command()
+async def rng(ctx, max: int):
+    await ctx.send(random.randint(1, max))
+
+@bot.command()
+async def rename(ctx, name):
+    await bot.user.edit(username=name)
+    print('rename ended')
+
+@rename.error
+async def rename_error(ctx, error):
+    print(error)
+    await ctx.send('i broke, plz fix me: \n' + '```{}```'.format(error))
+
+bot.run(TOKEN)
+
