c3348c5f591a6aac4ad3f12b57b16f1482a116bf
kc-kc <casey@theavilas.org>
2020-05-31 20:54:03 -0700

    Added approved admin_list for certain commands

--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@ from discord.ext import commands
 import random
 import os
 
-TOKEN = os.environ.get('TOKEN')
+admin_list = ['kc#0123', 'JustLuck#6936', 'jeff#1275', 'Dahling,SayAhhhhhhh#3332', '123cynhsu#6051']
 
+TOKEN = os.environ.get('TOKEN')
 bot = commands.Bot(command_prefix = '_')
+
 
 @bot.command()
 async def hello(ctx):
@@ -17,13 +19,19 @@ async def rng(ctx, max: int):
 
 @bot.command()
 async def rename(ctx, name):
-    await bot.user.edit(username=name)
-    print('rename ended')
+    if str(ctx.message.author) in admin_list:
+        await bot.user.edit(username=name)
+    else:
+        await ctx.send('ur not allowed to do that...')
+
+@bot.command()
+async def play(ctx, name):
+    await pass
 
 @rename.error
 async def rename_error(ctx, error):
     print(error)
-    await ctx.send('i broke, plz fix me: \n' + '```{}```'.format(error))
+    await ctx.send('```{}```'.format(error))
 
 bot.run(TOKEN)
 
