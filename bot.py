4999b62cad04abff5b45684da591e25f13f1e7b4
Casey Avila <casey@theavilas.org>
2020-06-18 23:29:48 -0700

    Made quotes consistent

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ ytdl_opts = {
     'source_address': '0.0.0.0',
 }
 
-ffmpeg_opts = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+ffmpeg_opts = '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
 ytdl = YoutubeDL(ytdl_opts)
 
 
